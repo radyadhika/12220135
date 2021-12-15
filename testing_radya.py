7ae03a576a1b5162c5c31da5f15c07a80d3575a9
radyadhika <95754803+radyadhika@users.noreply.github.com>
2021-12-16 02:37:42 +0700

    Add files via upload

---
+++ testing_radya.py
@@ -0,0 +1,231 @@
+"""
+Aplikasi Streamlit untuk menggambarkan statistik penumpang TransJakarta
+
+Sumber data berasal dari Jakarta Open Data
+Referensi API Streamlit: https://docs.streamlit.io/library/api-reference
+"""
+
+# import packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import json
+import streamlit as st
+
+# read dataset
+filepath = "produksi_minyak_mentah.csv"
+df = pd.read_csv(filepath)
+
+df_csv_clean = df.set_index("kode_negara")
+df_csv_clean.head()
+
+df_csv_clean = df_csv_clean.drop(["WLD","G20","OECD","OEU","EU28"])
+df_csv_clean.reset_index(drop=False, inplace=True)
+
+listkodecsv = []
+
+for i in list(df['kode_negara']):
+    if i not in listkodecsv:
+        listkodecsv.append(i)
+
+namajson = "kode_negara_lengkap.json"
+openjson = open(namajson)
+loadjson = json.load(openjson)
+
+list_kode_bersih = []
+for i in loadjson :
+    A = i.get('alpha-3')
+    list_kode_bersih.append(A)
+    if A not in listkodecsv:
+        list_kode_bersih.remove(A)
+
+df_json = pd.read_json(namajson)
+
+listnama = []
+listkodenegara = []
+listregion = []
+listsubregion = []
+
+for i in range(len(listkodecsv)):
+    for j in range(len(list(df_json['alpha-3']))):
+        if list(df_json['alpha-3'])[j] == listkodecsv[i] and list(df_json['name'])[j] not in listnama:
+            listnama.append(list(df_json['name'])[j])
+            listkodenegara.append(list(df_json['country-code'])[j])
+            listregion.append(list(df_json['region'])[j])
+            listsubregion.append(list(df_json['sub-region'])[j])
+
+df_json_clean = pd.DataFrame(list(zip(listnama, listkodecsv, listkodenegara, listregion, listsubregion)), columns=['NamaNegara', 'ISO3', 'KodeNegara', 'Region', 'SubRegion'])
+############### title ###############
+st.set_page_config(layout="wide")  # this needs to be the first Streamlit command called
+st.title("UAS 12220135")
+st.markdown("*Sumber data berasal dari [Jakarta Open Data](https://data.jakarta.go.id/dataset/data-jumlah-penumpang-trans-jakarta-tahun-2019-kpi)*")
+############### title ###############)
+
+############### sidebar ###############
+st.sidebar.title("Pengaturan")
+
+## User inputs on the control panel
+st.sidebar.subheader("Pengaturan konfigurasi tampilan")
+T = st.sidebar.number_input("Tahun Berapa", min_value=1970, max_value=2015, value=1990)
+B1 = st.sidebar.number_input("Banyak Negara", min_value=1, max_value=None, value=10)
+
+n_tampil = st.sidebar.number_input("Jumlah baris dalam tabel yang ditampilkan", min_value=1, max_value=None, value=10)
+############### sidebar ###############
+
+col1 = st.columns(1)
+col2 = st.columns(1)
+col3 = st.columns(1)
+col3opt1, col3opt2 = st.columns((1, 1))
+col4 = st.columns(1)
+col5a, col5b = st.columns(2)
+
+left_col, mid_col, right_col = st.columns(3)
+
+############### first column ###############
+col1.subheader("Tabel representasi data")
+col1.dataframe(df_csv_clean.head(n_tampil))
+############### first column ###############
+
+############### second column ###############
+col2.subheader("Jenis transportasi")
+
+for i in range(len(listnama)):
+    if listnama[i] == N:
+        kodenegarahuruf = listkodecsv[i]
+        kodenegaraangka = listkodenegara[i]
+        region = listregion[i]
+        subregion = listsubregion[i]
+
+# Membuat list baru untuk menampung data produksi negara dan tahunnya
+grafik1_tahun = []
+grafik1_produksi = []
+
+# Mengambil data produksi dan tahun berdasarkan negara yang dipilih pada
+# option dan memasukkannya ke list yang telah dibuat
+for i in range(len(list(df_csv_clean['kode_negara']))):
+    if kodenegarahuruf == list(df_csv_clean['kode_negara'])[i]:
+        grafik1_tahun.append(list(df_csv_clean['tahun'])[i])
+        grafik1_produksi.append(list(df_csv_clean['produksi'])[i])
+
+N = st.selectbox("Pilih Negara", listnama)
+
+fig, ax = plt.subplots(figsize=(8,5))
+plt.plot(grafik1_tahun, grafik1_produksi)
+plt.title('Grafik Produksi Negara ' + N, fontsize = 14)
+plt.legend([N])
+plt.xlabel('Tahun', fontsize = 12)
+plt.ylabel('Produksi', fontsize = 12)
+plt.grid(axis='y')
+
+col2.pyplot(fig)
+############### second column ###############
+
+############### third column ###############
+col3.subheader("Total penumpang perbulan")
+
+T = col3opt1.number_input("Tahun Berapa", min_value=1970, max_value=2015, value=1990)
+B1 = col3opt2.number_input("Banyak Negara", min_value=1, max_value=None, value=10)
+
+dftahun = df_csv_clean.loc[df_csv_clean['tahun'] == T].sort_values(by=['produksi'], ascending=False)
+dftahunbanyak = dftahun[:B1]
+
+negara2 = dftahunbanyak['kode_negara']
+produksi2 = dftahunbanyak['produksi']
+
+fig, ax = plt.subplots(figsize=(8,5))
+plt.bar(negara2, produksi2)
+plt.title('Grafik ' + str(B1) + ' Negara Produksi Terbesar pada Tahun ' + str(T), fontsize = 13)
+plt.xlabel('Negara', fontsize = 12)
+plt.ylabel('Produksi', fontsize = 12)
+plt.grid(axis='y')
+
+col3.pyplot(fig)
+############### third column ###############
+
+############### fourth column ###############
+B2 = col4.number_input("Banyak Negara", min_value=1, max_value=None, value=10)
+
+jumlah_produksi_kumulatif = []
+# Menjumlahkan produksi minyak tiap negara dan memasukkannya ke list_sum
+for i in listkodecsv:
+    a = df_csv_clean.loc[df_csv_clean['kode_negara'] == i, 'produksi'].sum()
+    jumlah_produksi_kumulatif.append(a)
+
+# Membuat dataframe baru dan diurutkan berdasarkan produksi kumulatif
+# minyak terbesar
+df3 = pd.DataFrame(list(zip(listkodecsv, jumlah_produksi_kumulatif)), columns=['kode_negara', 'produksi_kumulatif']).sort_values(by=['produksi_kumulatif'], ascending=False)
+df3 = df3 [:B2]
+
+negara3 = df3['kode_negara']
+produksikumul3 = df3['produksi_kumulatif']
+
+fig, ax = plt.subplots(figsize=(9,5))
+plt.bar(negara3, produksikumul3)
+plt.title('Grafik ' + str(B2) + ' Negara Produksi Kumulatif Terbesar', fontsize = 13)
+plt.xlabel('Negara', fontsize = 12)
+plt.ylabel('Produksi', fontsize = 12)
+plt.grid(axis='y')
+
+col4.pyplot(fig)
+############### fourth column ###############
+
+############### fifth column ###############
+col5a.subheader("Summary Tahun")
+dfsummary = pd.DataFrame(list(zip(listkodecsv, jumlah_produksi_kumulatif)), columns=['kode_negara', 'produksi_kumulatif']).sort_values(by=['produksi_kumulatif'], ascending=False)
+
+df_summary_clean = dfsummary.set_index("produksi_kumulatif")
+df_summary_clean.head()
+df_summary_clean = df_summary_clean.drop([0])
+df_summary_clean.reset_index(drop=False, inplace=True)
+
+print("Negara dengan Produksi Kumulatif Terbesar:")
+col5a.markdown("**Negara dengan Produksi Kumulatif Terbesar:**")
+print(df_summary_clean.iloc[0]['kode_negara'])
+col5a.markdown(df_summary_clean.iloc[0]['kode_negara'])
+print(df_summary_clean.iloc[0]['produksi_kumulatif'])
+col5a.markdown(df_summary_clean.iloc[0]['produksi_kumulatif'])
+
+print("\nNegara dengan Produksi Kumulatif Terkecil:")
+col5a.markdown("\n**Negara dengan Produksi Kumulatif Terkecil:**")
+print(df_summary_clean.iloc[len(df_summary_clean)-1]['kode_negara'])
+col5a.markdown(df_summary_clean.iloc[len(df_summary_clean)-1]['kode_negara'])
+print(df_summary_clean.iloc[len(df_summary_clean)-1]['produksi_kumulatif'])
+col5a.markdown(df_summary_clean.iloc[len(df_summary_clean)-1]['produksi_kumulatif'])
+
+df_summary_bersih = pd.DataFrame(list(zip(listkodecsv, jumlah_produksi_kumulatif)), columns=['kode_negara', 'produksi_kumulatif']).sort_values(by=['produksi_kumulatif'], ascending=False)
+df_summary_bersih = df_summary_bersih.set_index("kode_negara")
+df_summary_bersih.head()
+df_summary_bersih = df_summary_bersih.drop(["WLD","G20","OECD","OEU","EU28"])
+df_summary_bersih.reset_index(drop=False, inplace=True)
+
+print("\nNegara dengan Produksi Kumulatif Sama Dengan Nol:")
+col5a.markdown("\n**Negara dengan Produksi Kumulatif Sama Dengan Nol:**")
+negaranol = df_summary_bersih.iloc[len(df_summary_clean):len(dfsummary)]['kode_negara']
+produksinol = df_summary_bersih.iloc[len(df_summary_clean):len(dfsummary)]['produksi_kumulatif']
+dfnol = pd.DataFrame(list(zip(negaranol, produksinol)), columns=['kode_negara', 'produksi_kumulatif'])
+col5a.dataframe(dfnol.head)
+
+col5b.subheader("Summary Kumulatif")
+T2 = col5b.number_input("Tahun Berapa", min_value=1970, max_value=2015, value=1990)
+dftahun2 = df_csv_clean.loc[df_csv_clean['tahun'] == T2].sort_values(by=['produksi'], ascending=False)
+
+df_tahun_clean = dftahun2.set_index("produksi")
+df_tahun_clean.head()
+df_tahun_clean = df_tahun_clean.drop([0])
+df_tahun_clean.reset_index(drop=False, inplace=True)
+
+print("Negara dengan Produksi Minyak Terbesar pada Tahun " + str(T2) + ":")
+print(dftahun2.iloc[0]['kode_negara'])
+print(dftahun2.iloc[0]['produksi'])
+
+print("\nNegara dengan Produksi Minyak Terkecil pada Tahun " + str(T2) + ":")
+print(dftahun2.iloc[len(df_tahun_clean)-1]['kode_negara'])
+print(dftahun2.iloc[len(df_tahun_clean)-1]['produksi'])
+
+print("\nNegara dengan Produksi Minyak Sama Dengan Nol pada Tahun " + str(T2) + ":")
+negaranol2 = dftahun2.iloc[len(df_tahun_clean):len(dftahun2)]['kode_negara']
+produksinol2 = dftahun2.iloc[len(df_tahun_clean):len(dftahun2)]['produksi']
+dfnol2 = pd.DataFrame(list(zip(negaranol2, produksinol2)), columns=['kode_negara', 'produksi'])
+col5b.dataframe(dfnol2.head)
+############### fifth column ###############
